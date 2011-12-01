patmarion/PCL

commit b1735a1c906395659f0744bd556e7b443acc2d65

* fixed an older issue where the @is_dense@ flag was not set appropriately when reading data from a binary file (thanks Updog!)
git-svn-id: http://svn.pointclouds.org/pcl/branches/pcl-1.x@3193 a9d63959-f2ad-4865-b262-bf0e56cfafb6

io/src/pcd_io.cpp
@@ -80,7 +80,7 @@ pcl::PCDReader::readHeader (const std::string &file_name, sensor_msgs::PointClou
   cloud.data.clear ();
 
   // By default, assume that there are _no_ invalid (e.g., NaN) points
-  cloud.is_dense = true;
+  //cloud.is_dense = true;
 
   int nr_points = 0;
   std::ifstream fs;
@@ -253,11 +253,6 @@ pcl::PCDReader::readHeader (const std::string &file_name, sensor_msgs::PointClou
       if (line_type.substr (0, 6) == "HEIGHT")
       {
         sstream >> cloud.height;
-        /* Old, buggy behavior. is_dense does not represent "is_organized"
-        if (cloud.height == 1)
-          cloud.is_dense = false;
-        else
-          cloud.is_dense = true;*/
         continue;
       }
 
@@ -487,8 +482,6 @@ pcl::PCDReader::read (const std::string &file_name, sensor_msgs::PointCloud2 &cl
   /// ---[ Binary mode only
   /// We must re-open the file and read with mmap () for binary
   {
-    // Set the is_dense mode to false -- otherwise we would have to iterate over all points and check them 1 by 1
-    cloud.is_dense = false;
     // Open for reading
     int fd = pcl_open (file_name.c_str (), O_RDONLY);
     if (fd == -1)
@@ -618,6 +611,45 @@ pcl::PCDReader::read (const std::string &file_name, sensor_msgs::PointCloud2 &cl
   {
     PCL_ERROR ("[pcl::PCDReader::read] Number of points read (%d) is different than expected (%d)\n", idx, nr_points);
     return (-1);
+  }
+
+  // Get the X-Y-Z offset
+  int x_idx = -1, y_idx = -1, z_idx = -1;
+  for (size_t d = 0; d < cloud.fields.size (); ++d)                            
+  {
+    if (cloud.fields[d].name == "x")
+    {
+      x_idx = d;
+      continue;
+    }
+    if (cloud.fields[d].name == "y")
+    {
+      y_idx = d;
+      continue;
+    }
+    if (cloud.fields[d].name == "z")
+    {
+      z_idx = d;
+      continue;
+    }
+  }
+  // No sense in checking anything if the X-Y-Z fields are not found
+  if (x_idx == -1 || y_idx == -1 || z_idx == -1)
+    return (0);
+
+  cloud.is_dense = true;
+  // We want to make sure that is_dense is set accordingly on read, and must therefore check for invalid data here
+  float xval, yval, zval;
+  for (size_t i = 0; i < cloud.width * cloud.height; ++i)
+  {
+    memcpy (&xval, &cloud.data[cloud.fields[x_idx].offset], sizeof (float));
+    memcpy (&yval, &cloud.data[cloud.fields[y_idx].offset], sizeof (float));
+    memcpy (&zval, &cloud.data[cloud.fields[z_idx].offset], sizeof (float));
+    if (!pcl_isfinite (xval) || !pcl_isfinite (yval) || !pcl_isfinite (zval))
+    {
+      cloud.is_dense = false;
+      break;
+    }
   }
 
   return (0);

io/test/test_io.cpp
@@ -394,7 +394,7 @@ TEST (PCL, IO)
   EXPECT_NE ((int)res, -1);                               // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.width, cloud.width);    // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.height, cloud.height);  // test for loadPCDFile ()
-  EXPECT_EQ ((bool)cloud_blob.is_dense, false);  // @todo: let is_dense be false for now when reading from binary files
+  EXPECT_EQ ((bool)cloud_blob.is_dense, cloud.is_dense);
   EXPECT_EQ ((size_t)cloud_blob.data.size () * 2,         // PointXYZI is 16*2 (XYZ+1, Intensity+3)
               cloud_blob.width * cloud_blob.height * sizeof (PointXYZI));  // test for loadPCDFile ()
 
@@ -429,7 +429,7 @@ TEST (PCL, IO)
   EXPECT_NE ((int)res, -1);                               // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.width, cloud.width);    // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.height, cloud.height);  // test for loadPCDFile ()
-  EXPECT_EQ ((bool)cloud_blob.is_dense, false);
+  EXPECT_EQ ((bool)cloud_blob.is_dense, cloud.is_dense);
   EXPECT_EQ ((size_t)cloud_blob.data.size () * 2,         // PointXYZI is 16*2 (XYZ+1, Intensity+3)
               cloud_blob.width * cloud_blob.height * sizeof (PointXYZI));  // test for loadPCDFile ()
 
@@ -501,7 +501,7 @@ TEST (PCL, IO)
   EXPECT_NE ((int)res, -1);                               // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.width, cloud.width * cloud.height / 2);    // test for loadPCDFile ()
   EXPECT_EQ ((uint32_t)cloud_blob.height, 1);  // test for loadPCDFile ()
-  EXPECT_EQ ((bool)cloud_blob.is_dense, false);
+  EXPECT_EQ ((bool)cloud_blob.is_dense, cloud.is_dense);
   EXPECT_EQ ((size_t)cloud_blob.data.size () * 2,         // PointXYZI is 16*2 (XYZ+1, Intensity+3)
               cloud_blob.width * cloud_blob.height * sizeof (PointXYZI));  // test for loadPCDFile ()
 
@@ -594,7 +594,7 @@ TEST (PCL, PCDReaderWriter)
   reader.read ("test_pcl_io.pcd", cloud_blob);
   EXPECT_EQ ((uint32_t)cloud_blob.width, cloud.width);
   EXPECT_EQ ((uint32_t)cloud_blob.height, cloud.height);
-  EXPECT_EQ ((bool)cloud_blob.is_dense, false);   // @todo: let is_dense be false for now when reading from binary files
+  EXPECT_EQ ((bool)cloud_blob.is_dense, cloud.is_dense);
   //EXPECT_EQ ((size_t)cloud_blob.data.size () * 2,         // PointXYZI is 16*2 (XYZ+1, Intensity+3)
   //           cloud_blob.width * cloud_blob.height * sizeof (PointXYZI));  // test for loadPCDFile ()
   EXPECT_EQ ((size_t)cloud_blob.data.size (),         // PointXYZI is 16*2 (XYZ+1, Intensity+3)
@@ -854,7 +854,7 @@ TEST (PCL, LZF)
 
   EXPECT_EQ (cloud2.width, cloud.width);
   EXPECT_EQ (cloud2.height, cloud.height);
-  EXPECT_EQ (cloud2.is_dense, false);
+  EXPECT_EQ (cloud2.is_dense, cloud.is_dense);
   EXPECT_EQ (cloud2.points.size (), cloud.points.size ());
 
   for (size_t i = 0; i < cloud2.points.size (); ++i)
@@ -873,7 +873,7 @@ TEST (PCL, LZF)
 
   EXPECT_EQ (cloud2.width, blob.width);
   EXPECT_EQ (cloud2.height, blob.height);
-  EXPECT_EQ (cloud2.is_dense, false);
+  EXPECT_EQ (cloud2.is_dense, cloud.is_dense);
   EXPECT_EQ (cloud2.points.size (), cloud.points.size ());
 
   for (size_t i = 0; i < cloud2.points.size (); ++i)
@@ -919,7 +919,7 @@ TEST (PCL, LZFExtended)
 
   EXPECT_EQ (cloud2.width, blob.width);
   EXPECT_EQ (cloud2.height, blob.height);
-  EXPECT_EQ (cloud2.is_dense, false);
+  EXPECT_EQ (cloud2.is_dense, cloud.is_dense);
   EXPECT_EQ (cloud2.points.size (), cloud.points.size ());
 
   for (size_t i = 0; i < cloud2.points.size (); ++i)

io/tools/openni_grab_frame.cpp
@@ -52,57 +52,59 @@ bool set_file_name = false;
 class OpenNIGrabFrame
 {
   public:
-    OpenNIGrabFrame () : no_frame(true) {}
+    OpenNIGrabFrame () : no_frame (true)
+    {
+    }
 
     void 
     cloud_cb_ (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &cloud)
     {
-      if (no_frame)
+      if (!no_frame)
+        return;
+
+      std::string output_dir;
+      if (output_dir.empty ())
+        output_dir = ".";
+
+      std::stringstream ss;
+      if (noend || !set_file_name)
       {
-        std::string output_dir;
-        if (output_dir.empty ())
-          output_dir = ".";
-
-	std::stringstream ss;
-	if (noend || !set_file_name)
-	{
-          ss << output_dir << "/frame_" << boost::posix_time::to_iso_string(boost::posix_time::microsec_clock::local_time());
-          file = ss.str ();
-	}
-
-	if (strcmp (format.c_str(), "ascii") == 0)
-	{
-          w.writeASCII<pcl::PointXYZRGB> (file + ".pcd", *cloud);
-	  std::cerr << "Data saved in ASCII format to " << file << ".pcd" << std::endl;
-	}        
-	else if (strcmp (format.c_str(), "b") == 0)
-	{
-	  w.writeBinary<pcl::PointXYZRGB> (file + ".pcd", *cloud);
-	  std::cerr << "Data saved in BINARY format to " << file << ".pcd" << std::endl;
-	}
-	else if (strcmp (format.c_str(), "bc") == 0)
-	{
-	  w.writeBinaryCompressed<pcl::PointXYZRGB> (file + ".pcd", *cloud);
-	  std::cerr << "Data saved in BINARY COMPRESSED format to " << file << ".pcd" << std::endl;
-	}
-	  else if (strcmp (format.c_str(), "all") == 0)
-	{
-	  w.writeASCII<pcl::PointXYZRGB> (file + "_ascii.pcd", *cloud);
-	  w.writeBinary<pcl::PointXYZRGB> (file + "_binary.pcd", *cloud);
-	  w.writeBinaryCompressed<pcl::PointXYZRGB> (file + "_binary_compressed.pcd", *cloud);
-	  std::cerr << "Data saved as ASCII, BINARY and BINARY COMPRESSED to " << file << "_{ascii,binary,binary_compresed}.pcd" << std::endl;
-	}
-          
-	if (!noend)
-	  no_frame = false;
+        ss << output_dir << "/frame_" << boost::posix_time::to_iso_string (boost::posix_time::microsec_clock::local_time ());
+        file = ss.str ();
       }
+
+      if (strcmp (format.c_str (), "ascii") == 0)
+      {
+        w.writeASCII<pcl::PointXYZRGB> (file + ".pcd", *cloud);
+        std::cerr << "Data saved in ASCII format to " << file << ".pcd" << std::endl;
+      }        
+      else if (strcmp (format.c_str (), "b") == 0)
+      {
+        w.writeBinary<pcl::PointXYZRGB> (file + ".pcd", *cloud);
+        std::cerr << "Data saved in BINARY format to " << file << ".pcd" << std::endl;
+      }
+      else if (strcmp (format.c_str (), "bc") == 0)
+      {
+        w.writeBinaryCompressed<pcl::PointXYZRGB> (file + ".pcd", *cloud);
+        std::cerr << "Data saved in BINARY COMPRESSED format to " << file << ".pcd" << std::endl;
+      }
+        else if (strcmp (format.c_str (), "all") == 0)
+      {
+        w.writeASCII<pcl::PointXYZRGB> (file + "_ascii.pcd", *cloud);
+        w.writeBinary<pcl::PointXYZRGB> (file + "_binary.pcd", *cloud);
+        w.writeBinaryCompressed<pcl::PointXYZRGB> (file + "_binary_compressed.pcd", *cloud);
+        std::cerr << "Data saved as ASCII, BINARY and BINARY COMPRESSED to " << file << "_{ascii,binary,binary_compresed}.pcd" << std::endl;
+      }
+              
+      if (!noend)
+        no_frame = false;
     }
     
     void 
     run ()
     {
       // create a new grabber for OpenNI devices
-      pcl::Grabber* interface = new pcl::OpenNIGrabber();
+      pcl::Grabber* interface = new pcl::OpenNIGrabber ();
 
       // make callback function from member function
       boost::function<void (const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr&)> f =
@@ -125,12 +127,12 @@ class OpenNIGrabFrame
     void
     setOptions (std::string filename, std::string pcd_format, bool loop)
     {
-      if (strcmp (filename.c_str(), "") != 0)
+      if (strcmp (filename.c_str (), "") != 0)
       {  
-	set_file_name = true;
+        set_file_name = true;
 
-	size_t index = filename.find(".");
-	file = filename.substr(0, index);
+        size_t index = filename.find (".");
+        file = filename.substr (0, index);
       }
       noend = loop;
       format = pcd_format;
@@ -184,12 +186,12 @@ main (int argc, char** argv)
     
     // Command line parsing
     parse_argument (argc, argv, "-format", format);
-    if (find_argument(argc, argv, "-noend") != -1)
+    if (find_argument (argc, argv, "-noend") != -1)
       noend = true; 
   }
 
   v.setOptions (filename, format, noend);
   v.run ();
-  return 0;
+  return (0);
 }
 

commit 8a925474b4a84387f2da344629a8b4a919344f97

Fix bug in is_dense check introduced in r3193. (push to branches)
The dense check only checked the first point in the cloud.


git-svn-id: http://svn.pointclouds.org/pcl/branches/pcl-1.x@3283 a9d63959-f2ad-4865-b262-bf0e56cfafb6

io/src/pcd_io.cpp
@@ -642,9 +642,9 @@ pcl::PCDReader::read (const std::string &file_name, sensor_msgs::PointCloud2 &cl
   float xval, yval, zval;
   for (size_t i = 0; i < cloud.width * cloud.height; ++i)
   {
-    memcpy (&xval, &cloud.data[cloud.fields[x_idx].offset], sizeof (float));
-    memcpy (&yval, &cloud.data[cloud.fields[y_idx].offset], sizeof (float));
-    memcpy (&zval, &cloud.data[cloud.fields[z_idx].offset], sizeof (float));
+    memcpy (&xval, &cloud.data[i * cloud.point_step + cloud.fields[x_idx].offset], sizeof (float));
+    memcpy (&yval, &cloud.data[i * cloud.point_step + cloud.fields[y_idx].offset], sizeof (float));
+    memcpy (&zval, &cloud.data[i * cloud.point_step + cloud.fields[z_idx].offset], sizeof (float));
     if (!pcl_isfinite (xval) || !pcl_isfinite (yval) || !pcl_isfinite (zval))
     {
       cloud.is_dense = false;